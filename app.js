c915494dc8ba94350f643bcb7141f8838aff9cdd
Mohammed Wahba <best.pid@gmail.com>
2019-10-12 15:07:08 +0200

    adding usres&relations

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const {buildSchema} = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const dBUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-wuxiw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
@@ -26,11 +28,22 @@ app.use('/graphql', graphqlHttp({
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
             price: Float!
             date: String!
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
         }
 
         type RootQuery{
@@ -39,6 +52,7 @@ app.use('/graphql', graphqlHttp({
 
         type RootMutation{
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
         schema{
             query: RootQuery
@@ -63,15 +77,25 @@ app.use('/graphql', graphqlHttp({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price, // + is to number or Float , Ex: const a = '123'; console.log(typeof(+a));
-                date: new Date(args.eventInput.date)
-
+                date: new Date(args.eventInput.date),
+                creator: '5da1cefd41fd3405db9c0f2c' 
             });
-           
+            let createdEvent;
            return event
             .save()
             .then(result => { 
-                console.log(result);
-                return {...result._doc, _id: event._doc._id.toString()};// WHY 
+                createdEvent = {...result._doc, _id: event._doc._id.toString()};
+                return User.findById('5da1cefd41fd3405db9c0f2c');
+            })
+            .then(user => {
+                if (!user){
+                    throw new Error('User not found.')
+                }
+                user.createdEvents.push(event); // you cand pash event.id or only event, it will take id of event automaclly 
+                return user.save();   
+            })
+            .then(result => {
+                return createdEvent;
             })
             .catch(err => {
                  console.log(err);
@@ -80,6 +104,29 @@ app.use('/graphql', graphqlHttp({
                  
             });
 
+            
+        },
+        createUser: args => {
+           return User.findOne({email:args.userInput.email})
+                .then(user => {
+                if (user){
+                    throw new Error('User exists already.')
+                }
+                return bcrypt.hash(args.userInput.password, 12)
+                 })
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: args.userInput.email,
+                        password: hashedPassword
+                    });
+                    return user.save(); // save to db
+                })
+                .then(result => {
+                    return {...result._doc, password: null, _id: result.id}; // here is only reutrn to GraphQL not change anything in DB or return anything from db
+                })
+                .catch(err => {
+                    throw err;
+                });
             
         }
      },
